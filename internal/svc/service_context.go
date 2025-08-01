4edd789965f9ec3a2c2cc92c62b8470aa01246f2
ut <ut1221@icloud.com>
2025-08-01 15:53:36 +0800

    feat(random): 添加 Redis 配置并初始化连接_context.go 中添加 Redis 客户端初始化 - 修改 go.mod 和 go.sum 以包含 Redis 相关依赖

--- internal/svc/service_context.go
+++ internal/svc/service_context.go
@@ -1,15 +1,19 @@
 package svc
 
 import (
+	"github.com/zeromicro/go-zero/core/stores/redis"
 	"random/internal/config"
 )
 
 type ServiceContext struct {
 	Config config.Config
+	Rds    *redis.Redis
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	rds := redis.MustNewRedis(c.Redis)
 	return &ServiceContext{
 		Config: c,
+		Rds:    rds,
 	}
 }
